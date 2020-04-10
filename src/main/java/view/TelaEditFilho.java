1ee24ab2cc366d61d794e8e9c1c591b2313c040b
barabelo <47637808+barabelo@users.noreply.github.com>
2020-04-10 19:19:26 -0300

    Clean Build

--- src/main/java/view/TelaEditFilho.java
+++ src/main/java/view/TelaEditFilho.java
@@ -5,18 +5,24 @@
  */
 package view;
 
+import model.Baby;
+
 /**
  *
  * @author barab
  */
 public class TelaEditFilho extends javax.swing.JDialog {
 
+    private Baby baby;
+    
     /**
      * Creates new form dialogoEditFilho
      */
-    public TelaEditFilho(java.awt.Frame parent, boolean modal) {
+    public TelaEditFilho(java.awt.Frame parent, boolean modal, Baby baby) {
         super(parent, modal);
+        this.baby = baby;
         initComponents();
+        preencheCampos();
     }
 
     /**
@@ -187,47 +193,12 @@ public class TelaEditFilho extends javax.swing.JDialog {
                 || cmbSexo.getSelectedItem().equals("Selecione"));
     }
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(TelaEditFilho.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(TelaEditFilho.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(TelaEditFilho.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(TelaEditFilho.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-        //</editor-fold>
-
-        /* Create and display the dialog */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                TelaEditFilho dialog = new TelaEditFilho(new javax.swing.JFrame(), true);
-                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
-                    @Override
-                    public void windowClosing(java.awt.event.WindowEvent e) {
-                        System.exit(0);
-                    }
-                });
-                dialog.setVisible(true);
-            }
-        });
+    private void preencheCampos() {
+        txfAltura.setText(String.valueOf(baby.getHeight()));
+        txfPeso.setText(String.valueOf(baby.getWeight()));
+        txtIdentificador.setText(baby.getID());
+        txtNome.setText(baby.getName());
+        cmbSexo.setSelectedItem(baby.getSex());
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
